e52e1bcd8999f9cefc5a3a717bd43011736e22ec
Phavour-EBEN <ainooebenezer05@gmail.com>
2024-04-04 18:16:49 +0000

    Added some functionalities

--- src/main/java/com/example/calculator/HelloController.java
+++ src/main/java/com/example/calculator/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.calculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void onLogin(ActionEvent event) {
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+
+        Alert message = new Alert(Alert.AlertType.INFORMATION);
+        if(username.equals("admin") && password.equals("1234")){
+            message.setContentText("Welcome admin");
+            message.show();
+
+        }else {
+            message.setContentText("Incorrect username or password, please try again");
+            message.show();
+        }
+
+
     }
+
 }
